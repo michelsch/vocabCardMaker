c70f07082729c2c53892aad28223b5e83e6f0430
michaelsch92 <michel.schoemaker@gmail.com>
2016-02-26 12:18:25 -0800

    added readMe,cosmetic changes

--- template.js
+++ template.js
@@ -9,18 +9,8 @@ myApp.controller('myController', ['$scope', function($scope) {
   $scope.englishTitle = 'English Title';
 
   var vocabList = [
-'采访 interview (someone)',
-'测量 measure',
-'大使馆 embassy',
-'官方 official',
-'中产阶级 middle class',
-'沙漠 desert',
-'人造 man-made',
-'重工业 heavy industry',
-'人才流失 brain drain',
-'观念 concept',
-'插队 skip the line / cut the line',
-'浮躁 impulsive, short-sighted']
+'我 me',
+'你 you',]
 
   $scope.vocabWords = {};
 
